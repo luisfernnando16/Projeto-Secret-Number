220b4ca88e47341ea48cfd9f060a06304b4fabd9
Luis Fernando <luisfernnando16@gmail.com>
2024-11-18 10:18:03 -0300

    Second Commit - Limiting the number of user attempts

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import random
 
-user = int(input('Choose a number between 1 and 5: '))
 computer = random.randint(1, 5)
 
-if user == computer:
-    print('You chose the right number!')
-    print(f'The computer chose the number {computer} and you chose {user}')
-else:
-    print('You chose the wrong number!')
-    print(f'The computer chose the number {computer} and you chose {user}')
+vidas = 3
+
+while True:
+
+    user = int(input('Choose a number between 1 and 5: '))
+
+    if user == computer:
+        print('You chose the right number!')
+        print(f'The computer chose the number {computer} and you chose {user}')
+        break
+    else:
+        print('You chose the wrong number!')
+        print(f'The computer chose the number {computer} and you chose {user}')
+        
+    vidas -= 1
+
+    if vidas == 0:
+        print('Voce perdeu todas as suas vidas')
+        break
